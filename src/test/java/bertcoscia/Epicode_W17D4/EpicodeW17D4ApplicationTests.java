e0f0062700475932bdf8d3c4f3026a7360b5365a
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-09-05 14:17:25 +0200

    Initial commit

---
+++ src/test/java/bertcoscia/Epicode_W17D4/EpicodeW17D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package bertcoscia.Epicode_W17D4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeW17D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
